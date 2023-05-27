b761c5cb5dfdc3e43a6af5d816c13b4614d081dc
Furknozs <130183970+Furknozs@users.noreply.github.com>
2023-05-27 16:14:19 +0300

    Added Fish Movement3
    Added Fish Movement3

--- Assets/Scripts/LeftMovement.cs
+++ Assets/Scripts/LeftMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class LeftMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    BoxCollider2D box;
+    float groundWhidht;
+
     void Start()
     {
-        
+        box = GetComponent<BoxCollider2D>();
+        groundWhidht =box.size.x;
+      
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+       transform.position =new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        if (transform.position.x<= -groundWhidht)
+        {
+            transform.position = new Vector2(transform.position.x + 2 * groundWhidht, transform.position.y);
+        }
     }
 }
