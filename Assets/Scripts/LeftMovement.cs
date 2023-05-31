f0429d0fa3d0a1e0020b6d1b477e6b6e26e87879
Furknozs <130183970+Furknozs@users.noreply.github.com>
2023-05-31 16:31:58 +0300

    Added ObstacleSpawner
    Added ObstacleSpawner

--- Assets/Scripts/LeftMovement.cs
+++ Assets/Scripts/LeftMovement.cs
@@ -7,21 +7,36 @@ public class LeftMovement : MonoBehaviour
     public float speed;
     BoxCollider2D box;
     float groundWhidht;
-
+    float obstacleWhite;
     void Start()
     {
-        box = GetComponent<BoxCollider2D>();
-        groundWhidht =box.size.x;
-      
+        if (gameObject.CompareTag("Ground"))
+        {
+            box = GetComponent<BoxCollider2D>();
+            groundWhidht = box.size.x; //BoxCollider2D bileþenini alýr ve groundWhidht deðiþkenine bu bileþenin geniþlik deðerini atar.
+        }
+        else if (gameObject.CompareTag("Obstacle"))
+        {
+            obstacleWhite = GameObject.FindGameObjectWithTag("Column").GetComponent<BoxCollider2D>().size.x; //bileþeninin geniþlik deðeri atanýr.
+
+
+        }
+
     }
 
    
     void Update()
     {
-       transform.position =new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
-        if (transform.position.x<= -groundWhidht)
+       transform.position =new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y); // Bu satýr, nesnenin konumunu günceller. Nesnenin x konumu, mevcut x konumu (transform.position.x) üzerinden speed deðeri ile çarpýlarak ve zamanla çarpýlarak (Time.deltaTime) güncellenir. Bu sayede nesne belirli bir hýzda sola doðru hareket eder.
+
+        if (gameObject.CompareTag("Ground"))
         {
-            transform.position = new Vector2(transform.position.x + 2 * groundWhidht, transform.position.y);
+            if (transform.position.x <= -groundWhidht)
+            {
+                transform.position = new Vector2(transform.position.x + 2 * groundWhidht, transform.position.y); //Bu kod parçasý, bir nesnenin belirli bir hýzda sola doðru hareket etmesini ve bir sýnýra ulaþtýðýnda saða kaydýrýlmasýný saðlar.
+            }
         }
+
+        
     }
 }
